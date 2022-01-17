cc0fb1d715bd531e0308016d0bb5792cb34375b4
Hyeonmin Seon <hmsun.es@esgroup.net>
2022-01-17 22:13:27 +0900

    예제 시작

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::{thread, time::Duration};
+
+fn simulated_expensive_calculation(intensity: u32) -> u32 {
+    println!("Calculating...");
+    thread::sleep(Duration::from_secs(2));
+    intensity
+}
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    if intensity < 25 {
+        println!(
+            "오늘은 {}번의 팔굽혀펴기를 하세요!",
+            simulated_expensive_calculation(intensity)
+        );
+        println!(
+            "다음은 {}번의 윗몸일으키기를 하세요!",
+            simulated_expensive_calculation(intensity)
+        );
+    } else {
+        if random_number == 3 {
+            println!("오늘은 수분을 충분히 섭취하며 쉬세요!");
+        } else {
+            println!(
+                "오늘은 {}분간 달리기를 하세요!",
+                simulated_expensive_calculation(intensity)
+            );
+        }
+    }
+}
+
+fn main() {
+    let simulated_user_specificated_value = 10;
+    let simulated_random_number = 7;
+
+    generate_workout(simulated_user_specificated_value, simulated_random_number);
+}
