d03e6dd740d13bba556a3b7b2a5d77e0718e6f03
Hyeonmin Seon <hmsun.es@esgroup.net>
2022-01-17 22:14:46 +0900

    한번만 호출하게 했지만 호출이 필요없는경우에도 호출하게 된다

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,16 @@ fn simulated_expensive_calculation(intensity: u32) -> u32 {
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
+    let expensive_result = simulated_expensive_calculation(intensity);
+
     if intensity < 25 {
-        println!(
-            "오늘은 {}번의 팔굽혀펴기를 하세요!",
-            simulated_expensive_calculation(intensity)
-        );
-        println!(
-            "다음은 {}번의 윗몸일으키기를 하세요!",
-            simulated_expensive_calculation(intensity)
-        );
+        println!("오늘은 {}번의 팔굽혀펴기를 하세요!", expensive_result);
+        println!("다음은 {}번의 윗몸일으키기를 하세요!", expensive_result);
     } else {
         if random_number == 3 {
             println!("오늘은 수분을 충분히 섭취하며 쉬세요!");
         } else {
-            println!(
-                "오늘은 {}분간 달리기를 하세요!",
-                simulated_expensive_calculation(intensity)
-            );
+            println!("오늘은 {}분간 달리기를 하세요!", expensive_result);
         }
     }
 }
